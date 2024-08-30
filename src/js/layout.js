bfdae38d53aabb8f96568c72857e63cb63665632
Alvaro Nicolas Ortiz <160544793+ANicolasOrtiz@users.noreply.github.com>
2024-08-30 19:19:16 +0000

    faltan detalles y errores por corregir

--- src/js/layout.js
+++ src/js/layout.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
 import { Single } from "./views/single";
 import injectContext from "./store/appContext";
 
@@ -21,7 +20,6 @@ const Layout = () => {
 					<Navbar />
 					<Routes>
 						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
 						<Route path="/single/:theid" element={<Single />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
